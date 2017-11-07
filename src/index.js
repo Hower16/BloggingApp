3d8e7968c4b7c81c7e4110d3815e642efa1337e4
Hower16 <miky1216@gmail.com>
2017-11-06 18:37:34 -0600

    Added functionality to render a list of posts

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import promise from 'redux-promise';
 
 import reducers from './reducers';
 import PostsIndex from './components/posts_index';
+import PostsShow from './components/posts_show';
+import PostsNew from './components/posts_new';
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
