3750ec15a1f211f20230f5dbc5f75457ca549f3c
Hower16 <miky1216@gmail.com>
2017-11-07 10:15:42 -0600

    Begin implementation of Redux Form

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+
 
 class PostsNew extends Component {
   render() {
