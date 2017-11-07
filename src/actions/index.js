fbb45eaf21cb6a5a8de7cbf70af39f37fb40c993
Hower16 <miky1216@gmail.com>
2017-11-07 13:15:49 -0600

    Added create new post functionality

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,12 @@
 import axios from 'axios';
 
 export const FETCH_POSTS = 'fetch_posts';
+export const FETCH_POST = 'fetch_post';
+export const CREATE_POST = 'create_post';
+export const DELETE_POST = 'delete_post';
 
-const ROOT_URL = "http://reduxblog.herokuapp.com/api";
-const API_KEY = "?key=MIKE123";
+const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
+const API_KEY = '?key=MIKE123';
 
 export function fetchPosts(){
   const request = axios.get(`${ROOT_URL}/posts${API_KEY}`);
@@ -14,8 +17,10 @@ export function fetchPosts(){
   };
 }
 
-export function createPost(values) {
-  const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, values);
+export function createPost(values, callback) {
+  const request = axios
+    .post(`${ROOT_URL}/posts${API_KEY}`, values)
+    .then(() => callback());
 
   return {
     type: CREATE_POST,
