8b7adfa4fc605d8208fe01613cb146c9b342b158
Hower16 <miky1216@gmail.com>
2017-11-07 14:26:41 -0600

    Added functionality to delete a post

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -9,10 +9,33 @@ class PostsShow extends Component {
     this.props.fetchPost(id);
   }
 
+  onDeleteClick() {
+    const { id } = this.props.match.params;
+
+    this.props.deletePost(id, () => {
+      this.props.history.push("/");
+    });
+  }
+
   render() {
+    const { post } = this.props;
+
+    if (!post) {
+      return <div>Loading...</div>;
+    }
+
     return (
       <div>
-        Posts Show
+        <Link to="/">Back To Index</Link>
+        <button
+          className="btn btn-danger pull-xs-right"
+          onClick={this.onDeleteClick.bind(this)}
+        >
+          Delete Post
+        </button>
+        <h3>{post.title}</h3>
+        <h6>Categories: {post.categories}</h6>
+        <p>{post.content}</p>
       </div>
     );
   }
